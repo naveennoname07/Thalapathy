5adb5c89c39f3ac64385139d12cf443b1076c21d
CrazyBossAbhishek <93928612+200920082007@users.noreply.github.com>
2022-04-27 14:28:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -845,6 +845,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         o=await n.edit("● ● ●")
         await asyncio.sleep(1)
         await o.delete()
+        await query.answer("𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝗍𝗈 𝗆𝗒 𝖧𝖾𝗅𝗉 𝗆𝗈𝖽𝗎𝗅𝖾")
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
