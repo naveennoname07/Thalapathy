b7f53a035d9a80edef66f17e48609241deeeae15
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-04-30 06:59:57 +0530

    Update hacked.py

--- plugins/hacked.py
+++ plugins/hacked.py
@@ -1,48 +1,38 @@
-import asyncio
 from pyrogram import Client,filters
+import logging
+import os
+
+from geopy.geocoders import Nominatim
+
+from plugins.shazam.function.pluginhelpers import edit_or_reply
+from plugins.shazam.function.basic_helpers import get_text
 
 
-@Client.on_message(filters.command(["hack", "heck"]))
-async def hack_func(message):
-    user = await message.client.get_user_dict(message.from_user.id)
-    heckerman = user['mention']
-    animation_chars = [
-        "```Connecting To Private Server \\```",
-        "```Connecting To Private Server |```",
-        "```Connecting To Private Server /```",
-        "```Connecting To Private Server \\```",
-        "```Connection Established ```",
-        "```Target Selected```",
-        "```Backdoor Found In Target```",
-        "```Trying To Hack```",
-        "```Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒```",
-        "```Hacking... 70%\n█████████████████▒▒▒▒▒```",
-        "```Hacking... 88%\n█████████████████████▒```",
-        "```Hacking... 100%\n███████████████████████```",
-        "```Preparing Data... 1%\n▒██████████████████████```",
-        "```Preparing Data... 14%\n████▒██████████████████```",
-        "```Preparing Data... 30%\n████████▒██████████████```",
-        "```Preparing Data... 55%\n████████████▒██████████```",
-        "```Preparing Data... 72%\n████████████████▒██████```",
-        "```Preparing Data... 88%\n████████████████████▒██```",
-        "```Prepared Data... 100%\n███████████████████████```",
-        "```Uploading Data to Server... 12%\n███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Uploading Data to Server... 44%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
-        "```Uploading Data to Server... 68%\n███████████████▒▒▒▒▒▒▒▒```",
-        "```Uploading Data to Server... 89%\n████████████████████▒▒▒```",
-        "```Uploaded Data to Server... 100%\n███████████████████████```",
-        "**User Data Upload Completed:** Target's User Data Stored "
-        "at `downloads/victim/telegram-authuser.data.sql`",
-    ]
-    hecked = (f"**Targeted Account Hacked**\n\n```Pay 69$ To``` {heckerman}``` "
-              "To Remove This Hack```")
-    max_ani = len(animation_chars)
-    for i in range(max_ani):
-        await asyncio.sleep(2)
-        await message.edit(animation_chars[i % max_ani])
-    await message.edit(hecked)
+GMAPS_LOC = "https://maps.googleapis.com/maps/api/geocode/json"
+
+
+@Client.on_message(filters.command(["gps", "locate"]))
+async def gps(client, message):
+    engine = message.Engine
+    pablo = await edit_or_reply(message, engine.get_string("PROCESSING"))
+    args = get_text(message)
+    if not args:
+        await pablo.edit(engine.get_string("INPUT_REQ").format("Location"))
+        return
+    try:
+        geolocator = Nominatim(user_agent="FridayUB")
+        location = args
+        geoloc = geolocator.geocode(location)
+        longitude = geoloc.longitude
+        latitude = geoloc.latitude
+    except Exception as e:
+        logging.info(e)
+        await pablo.edit(engine.get_string("GPS_2"))
+        return
+    gm = "https://www.google.com/maps/search/{},{}".format(latitude, longitude)
+    await client.send_location(message.chat.id, float(latitude), float(longitude))
+    await pablo.reply(
+        "Open with: [Google Maps]({})".format(gm),
+        disable_web_page_preview=False,
+    )
+    await pablo.delete()
