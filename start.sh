a0e6605a93aa6edbb53b451bc1a15c80eb0631d5
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-05-05 17:37:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-echo "Cloning main Repository"
-git clone -b main https://github.com/200920082007/TigerShroff.git /TigerShroff
-cd /TigerShroff
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/200920082007/TigerShroff.git /massilama
+cd /massilama
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Test....🔥"
+echo "Starting Bot, Please Wait..."
 python3 bot.py
