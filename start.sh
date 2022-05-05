9da069122e026bb66758b576575af61a88faf45d
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-05-05 17:43:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/200920082007/TigerShroff.git /massilama
+git clone -b master https://github.com/200920082007/massssssss4.git /massilama
 cd /massilama
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
