1d58788a00ea116261a8e5bc343ad8091cf2a449
naveennoname07 <85923611+naveennoname07@users.noreply.github.com>
2022-05-15 16:24:09 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,3 +1,15 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
