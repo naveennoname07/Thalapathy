6c6b665225dd53512bd05e6c62de156535bb3226
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-05-22 09:57:41 +0530

    Update __init___.py

--- wbb/__init___.py
+++ wbb/__init___.py
@@ -1,5 +1,27 @@
 from info import DATABASE_URI as MONGO_URL
 
+
+class Log:
+    def __init__(self, save_to_file=False, file_name="wbb.log"):
+        self.save_to_file = save_to_file
+        self.file_name = file_name
+
+    def info(self, msg):
+        print(f"[+]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[INFO]({time.ctime(time.time())}): {msg}\n")
+
+    def error(self, msg):
+        print(f"[-]: {msg}")
+        if self.save_to_file:
+            with open(self.file_name, "a") as f:
+                f.write(f"[ERROR]({time.ctime(time.time())}): {msg}\n")
+
+
+log = Log(True, "bot.log")
+
+
 log.info("Initializing MongoDB client")
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client.wbb
